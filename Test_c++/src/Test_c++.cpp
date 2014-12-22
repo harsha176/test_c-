cd16cc617961abaf92c7dde5fd4d35b50d193f32
harsha <harsha176@gmail.com>
2014-12-22 08:52:38 -0800

    Initial import of the project.
    It is mainly used for understanding C++.

---
+++ Test_c++/src/Test_c++.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Test_c++.cpp
+// Author      : Harshavardhan Malipatel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void) {
+	cout << "OM Tat Sat!!!" << endl;
+	cout << "Om Shanti Shanti Shanti" << endl;
+	return EXIT_SUCCESS;
+}
