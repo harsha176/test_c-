71f1eaa1d97e430b213af99020042c41217b826c
harsha <harsha176@gmail.com>
2014-12-22 08:54:17 -0800

    Updated with complete surrender Mantra

--- Test_c++/src/Test_c++.cpp
+++ Test_c++/src/Test_c++.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 int main(void) {
-	cout << "OM Tat Sat!!!" << endl;
+	cout << "Hari OM Tat Sat!!!" << endl;
 	cout << "Om Shanti Shanti Shanti" << endl;
 	return EXIT_SUCCESS;
 }
